11b28bd4e653a16980e1e284856adc82cfa6ebfe
Karl W Broman <broman@wisc.edu>
2024-11-27 23:20:02 +0000

    version 0.6

--- inst/doc/mbmixture.R
+++ inst/doc/mbmixture.R
@@ -2,7 +2,7 @@
 library(knitr)
 opts_chunk$set(fig.width=7, fig.height=4.5,
                dev.args=list(pointsize=16))
-scipen <- options("scipen")
+scipen <- getOption("scipen")
 options(scipen=10)
 
 ## ----load_lib_and_data--------------------------------------------------------
@@ -25,7 +25,7 @@ mbmix_loglik(mbmixdata, p=0.74, e=0.002)
 attr(mle_w_SE, "SE")
 
 ## ----bootstrap, eval=FALSE----------------------------------------------------
-#  bootstrap_se <- bootstrapSE(mbmixdata, 1000)
+# bootstrap_se <- bootstrapSE(mbmixdata, 1000)
 
 ## ----bootstrap_result, echo=FALSE---------------------------------------------
 c(p = 0.000351360951177482, err = 0.0000272304773558442)
